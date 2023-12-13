a264dd517c51683b419c1f22185c9eafb131664e
MrZebarth <kris.zebarth@ocsb.ca>
2023-12-13 10:08:21 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome! How old are you?");
+		int age = in.nextInt();
+		System.out.println("What's your favourite colour?");
+		in.nextLine();
+		String colour = in.nextLine();
+	}
+
+}
